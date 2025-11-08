5c82a428f853f396a10ca414e180029519dd37bc
Mraimundo <mdumundo18@gmail.com>
2025-11-08 16:48:25 -0300

    feat: :sparkles: add Google Maps API key configuration and ticket management page

---
+++ src/app/ticket-management/page.tsx
@@ -0,0 +1,7 @@
+export default function TicketManagement() {
+  return (
+    <main>
+      <h1>Bem Vindo a paginade Ticket</h1>
+    </main>
+  );
+}
--- src/env/index.ts
+++ src/env/index.ts
@@ -2,10 +2,12 @@ import { z } from 'zod';
 
 const envSchema = z.object({
   NEXT_PUBLIC_BASE_URL: z.string().url(),
+  NEXT_PUBLIC_GOOGLE_MAPS_API_KEY: z.string().min(1),
 });
 
 const _env = envSchema.safeParse({
   NEXT_PUBLIC_BASE_URL: process.env.NEXT_PUBLIC_BASE_URL,
+  NEXT_PUBLIC_GOOGLE_MAPS_API_KEY: process.env.NEXT_PUBLIC_GOOGLE_MAPS_API_KEY,
 });
 
 if (!_env.success) {
@@ -15,4 +17,5 @@ if (!_env.success) {
 
 export const env = {
   NEXT_PUBLIC_BASE_URL: _env.data.NEXT_PUBLIC_BASE_URL,
+  NEXT_PUBLIC_GOOGLE_MAPS_API_KEY: _env.data.NEXT_PUBLIC_GOOGLE_MAPS_API_KEY,
 };
